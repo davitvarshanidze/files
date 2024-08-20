6cb7700b8fd7db7f0f95bbb1afab41af7fb78081
David Varshanidze <davittvarshanidze@gmail.com>
2024-08-20 16:59:20 +0400

    Create disabled.lua

---
+++ nvim/lua/plugins/disabled.lua
@@ -0,0 +1,9 @@
+return {
+  { "akinsho/bufferline.nvim", enabled = false },
+  { "rcarriga/nvim-notify", enabled = false },
+  { "echasnovski/mini.icons", enabled = false },
+  { "nvimdev/dashboard-nvim", enabled = false },
+  { "goolord/alpha-nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
+  { "folke/which-key.nvim", enabled = false },
+}
